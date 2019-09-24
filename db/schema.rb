acfa98d98cc189c13458f47d2768dac9fe214c31
Lucas Martins <lucas465@hotmail.com>
2019-09-23 23:50:22 -0300

    Adds url shortener

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_923_232_552) do
+ActiveRecord::Schema.define(version: 2019_09_24_194049) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,4 +23,21 @@ ActiveRecord::Schema.define(version: 20_190_923_232_552) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
+  create_table "shortened_urls", force: :cascade do |t|
+    t.integer "owner_id"
+    t.string "owner_type", limit: 20
+    t.text "url", null: false
+    t.string "unique_key", limit: 10, null: false
+    t.string "category"
+    t.integer "use_count", default: 0, null: false
+    t.datetime "expires_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category"], name: "index_shortened_urls_on_category"
+    t.index ["owner_id", "owner_type"], name: "index_shortened_urls_on_owner_id_and_owner_type"
+    t.index ["unique_key"], name: "index_shortened_urls_on_unique_key", unique: true
+    t.index ["url"], name: "index_shortened_urls_on_url"
+  end
+
 end
--- script/start.sh
+++ script/start.sh
@@ -9,18 +9,18 @@ done
 >&2 echo "Postgres is up - executing commands..."
 
 if bundle exec rails db:exists; then
-	>&2 echo "Database exists, only migrating it..."
+	>&2 echo "Database exists, running migrations..."
 	bundle exec rails db:migrate
 else
-	>&2 echo "Database doesn't exist, creating and migrating it..."
+	>&2 echo "Database doesn't exist, creating and running migrations..."
 	bundle exec rails db:create
 	bundle exec rails db:schema:load
 fi
 
 if [ -f $pidfile ] ; then
-	>&2 echo 'Server PID file already exists. Removing it...'
+	>&2 echo 'Removing server PID file...'
 	rm $pidfile
 fi
 
 >&2 echo 'Running processes...'
-bundle exec rails s -p 3000 -b 0.0.0.0
+bundle exec rails s -p $APP_PORT -b $APP_HOST
